b4e886556b030e53b28f78a4a6f691c3b6f170ed
Lizachann <liza.chan@student.cadt.edu.kh>
2023-04-24 16:23:37 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="Booking" element={<Booking />} />
+        <Route path="/hi" element = {<Homepage/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
