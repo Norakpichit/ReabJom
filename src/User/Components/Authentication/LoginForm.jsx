794fb6c99e064229cfce8b80a9ecfd7c074a96ec
Sanmon17 <120361089+Sanmon17@users.noreply.github.com>
2023-04-27 11:37:50 +0700

    designed login

--- src/User/Components/Authentication/LoginForm.jsx
+++ src/User/Components/Authentication/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import withus_black from "../../../assets/withus_black.png";
 
 export default function LoginForm() {
     const [username, setUsername] = useState('');
@@ -19,15 +20,16 @@ export default function LoginForm() {
 
     return (
     <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className='m-auto !z-5 relative flex flex-col rounded-[20px] max-w-[300px] md:max-w-[400px] bg-blue-200 bg-clip-border shadow-3xl shadow-shadow-500 flex flex-col w-full !p-6 3xl:p-![18px] bg-white undefined'>    
+        <div className='m-auto !z-5 relative flex flex-col rounded-[20px] max-w-[300px] md:max-w-[400px] bg-blue-200 bg-clip-border shadow-3xl shadow-shadow-500 flex flex-col w-full !p-6 3xl:p-![18px] undefined'>    
             <div className="mb-4">
+                <img src={withus_black} alt="" className=" h-12 m-auto" /> 
                 <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
                     Username
                 </label>
                 <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
-                placeholder="Username"
+                placeholder="Email or Phone Number"
                 value={username}
                 onChange={handleUsernameChange}
                 />
@@ -50,8 +52,9 @@ export default function LoginForm() {
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline\"
                 type="submit"
                 >
-                    Sign In
+                    Login
                 </button>
+
                 <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
                     Forgot Password?
                 </a>
