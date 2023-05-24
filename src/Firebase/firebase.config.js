8165bbcc67e04830d7b67e63a8acc8d117ddca3a
Sanmon17 <chingsan1717@gmail.com>
2023-05-25 01:54:48 +0700

    change firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -20,12 +20,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDx-tewL7tyDnay4KNq396C0GE3miw3Lr8",
-  authDomain: "reabjom-f7d82.firebaseapp.com",
-  projectId: "reabjom-f7d82",
-  storageBucket: "reabjom-f7d82.appspot.com",
-  messagingSenderId: "948338939941",
-  appId: "1:948338939941:web:38703375b68f54f1e347cc"
+  apiKey: "AIzaSyCzBPnrRRIJh2qpd2P_r-WNB99qiGPurZY",
+  authDomain: "reabjom-d3629.firebaseapp.com",
+  projectId: "reabjom-d3629",
+  storageBucket: "reabjom-d3629.appspot.com",
+  messagingSenderId: "664813287365",
+  appId: "1:664813287365:web:dc18ae29c3945af8bb3dac",
+  measurementId: "G-LH8MMNKN5L",
 };
 
 // Initialize Firebase
