bfece8565d3d27d8bc05db840a50f8a3d7603150
Sanmon17 <120361089+Sanmon17@users.noreply.github.com>
2023-05-24 21:10:48 +0700

    add to test

--- src/User/Components/Authentication/Signup1stForm.jsx
+++ src/User/Components/Authentication/Signup1stForm.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import withus_black from "../../../assets/withus_black.png";
 import { Link } from 'react-router-dom';
-import {BsTelephoneFill} from "react-icons/bs";
-import { BsFillShieldLockFill } from 'react-icons/bs';
+import {BsTelephoneFill,BsFillShieldLockFill} from "react-icons/bs";
 import {CgSpinner} from "react-icons/cg";
 import PhoneInput from 'react-phone-input-2'
 import "react-phone-input-2/lib/style.css";
 import OTPInput from "otp-input-react";
+// import { auth } from '../../../Firebase/firebase.config';
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+// import { toast, Toaster } from "react-hot-toast";
 
 export default function Signup1stForm(){
+    // const auth = getAuth();
     const [switchSignup, setSwitchSignup] = useState(false);
     const [username, setUsername] = useState('');
     const [isFormValid, setIsFormValid] = useState(false);
     const [ph, setPh] = useState("");
     const [otp,setOtp] = useState("");
     const [loading, setLoading] = useState(false);
-    const [showOTP, setShowOTP] = useState(true);
+    const [showOTP, setShowOTP] = useState(false);
+    // const [user,setUser] = useState(null);
     
     useEffect(() => {
         console.log(username);
@@ -30,32 +34,61 @@ export default function Signup1stForm(){
         setUsername(event.target.value);
         validateEmailForm();
     };
-
-    const validatePhoneForm = () => {
-        const usernamePattern = /^([0]{1}[1-9]{1}[0-9]{6})|([0]{1}[1-9]{1}[0-9]{7})$/;
-        if (username && usernamePattern.test(username)) {
-            setIsFormValid(true);
-        } else {
-            setIsFormValid(false);
-        }
-    };
-    
-    const validateEmailForm = () => {
-        const usernamePattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$$/;
-        if (username && usernamePattern.test(username)) {
-            setIsFormValid(true);
-        } else {
-            setIsFormValid(false);
-        }
-    };
       
     const handleClick = () => {
         setSwitchSignup(!switchSignup);
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    function onCaptchVerify() {
+        if (!window.recaptchaVerifier) {
+            window.recaptchaVerifier = new RecaptchaVerifier(
+                'recaptcha-container',
+                {
+                'size': "invisible",
+                'callback': (response) => {
+                    // onSignup();
+                },
+                'expired-callback': () => {},
+                },auth
+            );
+        }
     }
+    
+    // function onSignup() {
+    //     setLoading(true);
+    //     onCaptchVerify();
+    
+    //     const appVerifier = window.recaptchaVerifier;
+    //     const formatPh = "+" + ph;
+    
+    //     signInWithPhoneNumber(auth, formatPh, appVerifier)
+
+    //     .then((confirmationResult) => {
+    //         window.confirmationResult = confirmationResult;
+    //         setLoading(false);
+    //         setShowOTP(true);
+    //         // toast.success("OTP sended successfully!");
+    //     })
+    //         .catch((error) => {
+    //             console.log(error);
+    //             setLoading(false);
+    //     });
+    // }
+    
+    // function onOTPVerify() {
+    //     setLoading(true);
+    //     window.confirmationResult
+    //         .confirm(otp)
+    //         .then(async (res) => {
+    //             console.log(res);
+    //             setUser(res.user);
+    //             setLoading(false);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         setLoading(false);
+    //     });
+    // }
     
     return (
         <section className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -63,7 +96,7 @@ export default function Signup1stForm(){
                     <>
                         <div className='m-auto !z-5 relative flex flex-col rounded-[20px] max-w-[300px] md:max-w-[400px] bg-blue-200 bg-clip-border shadow-3xl shadow-shadow-500 flex flex-col w-full !p-6 3xl:p-![18px] undefined'>    
                             <img src={withus_black} alt="" className=" h-12 m-auto" /> 
-                            <label className='text-center block mb-3 mt-1'> Create an Account </label>
+                            <label htmlFor="otp" className='text-center block mb-3 mt-1'> Create an Account </label>
                             <div className='bg-white text-blue-500 w-fit mb-3 mx-auto p-4 rounded-full'>
                                 <BsFillShieldLockFill size={30} />
                             </div>
@@ -71,7 +104,7 @@ export default function Signup1stForm(){
                             <h1 className="text-gray-700 font-bold mb-3 mx-auto"> Enter Your OTP! </h1>
                             <OTPInput value={otp} onChange={setOtp} OTPLength={6} otpType="number" disabled={false} autoFocus className="otp-container"></OTPInput>
                             
-                            <button className="mt-4 bg-blue-500 w-full flex gap-1 justify-center hover:bg-blue-700 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline">
+                            <button className="drop-shadow-md mt-4 bg-blue-500 w-full flex gap-1 justify-center hover:bg-blue-700 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline">
                                 {
                                     loading && <CgSpinner size={20} 
                                     className='mt-1 animate-spin'/>
